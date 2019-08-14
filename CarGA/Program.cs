c557f270ac78dc293a689acdc9077c5a7e2cc436
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-14 12:54:54 -0700

    Agregar archivos de proyecto.

---
+++ CarGA/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace CarGA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagen = "C:\\Users\\alumno\\Pictures\\Valentin-Elizalde.jpg";
+
+            Image<Bgr, byte> img = new Image<Bgr, byte>(imagen);
+            CvInvoke.Imshow("Imagen", img);
+
+            CvInvoke.WaitKey(0);
+
+
+        }
+    }
+}
